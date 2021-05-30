60a953484547f3e9a27139902b66ffbd6b6cbdc3
timmywheels <tim@timwheeler.com>
2021-05-30 10:08:54 -0400

    refactor godaddy service

--- src/services/godaddy.js
+++ src/services/godaddy.js
@@ -3,17 +3,16 @@ const fetch = require("node-fetch");
 const batchRequest = require("batch-request-js");
 
 const GODADDY_BATCH_SIZE = 1;
-const GODADDY_RATE_LIMIT_DELAY = 0;
+const GODADDY_RATE_LIMIT_DELAY = 1100;
 
 const domains = getDomains();
-let domainsForwardedCount = 0;
+let domainUpdatedCount = 0;
 
 const calculateProgress = (domainsUpdated, totalDomains) => {
     return ((domainsUpdated / totalDomains) * 100).toFixed(2);
 }
 
 const domainForwardingUpdate = async (domain) => {
-    domainsForwardedCount++;
     const payload = {
         type: "REDIRECT_TEMPORARY",
         url: `https://${options.domainPageUsername}.domainpage.io/${domain}`
@@ -28,11 +27,6 @@ const domainForwardingUpdate = async (domain) => {
         body: JSON.stringify(payload)
     })
     if (!res.ok) return console.error(`Error forwarding domain: ${domain} - [${res.status}]: ${res.statusText}`);
-    console.info('----------')
-    console.info(`[${calculateProgress(domainsForwardedCount, domains.length)}%]`)
-    console.info(`Domain: ${domain}`)
-    console.info(`Redirect: https://${options.domainPageUsername}.domainpage.io/${domain}`)
-    console.info('----------\n')
 }
 
 const domainTxtRecordUpdate = async (domain) => {
@@ -54,9 +48,21 @@ const domainTxtRecordUpdate = async (domain) => {
     if (!res.ok) return console.error(`Error updating TXT record for domain: ${domain} - [${res.status}]: ${res.statusText}`);
 }
 
+const domainUpdate = async (domain) => {
+    domainUpdatedCount++;
+    domainForwardingUpdate(domain).catch(err => err);
+    domainTxtRecordUpdate(domain).catch(err => err);
+    console.info('----------')
+    console.info(`[${calculateProgress(domainUpdatedCount, domains.length)}% complete - ${domains.length - domainUpdatedCount} remaining]`)
+    console.info(`Domain: ${domain}`)
+    console.info(`Redirect: https://${options.domainPageUsername}.domainpage.io/${domain}`)
+    console.info(`TXT Record: domainpage-owner=${options.domainPageUsername}`)
+    console.info('----------\n')
+}
+
 const bulkDomainUpdate = async () => {
-    const { error: domainForwardingError, data: domainForwardingData } = await batchRequest(domains, domainForwardingUpdate, { batchSize: GODADDY_BATCH_SIZE, delay: GODADDY_RATE_LIMIT_DELAY });
-    const { error: domainTxtError, data: domainTxtData } = await batchRequest(domains, domainTxtRecordUpdate, { batchSize: GODADDY_BATCH_SIZE, delay: GODADDY_RATE_LIMIT_DELAY });
+    const { error, data } = await batchRequest(domains, domainUpdate, { batchSize: GODADDY_BATCH_SIZE, delay: GODADDY_RATE_LIMIT_DELAY });
+    if (error) console.error(`Error updating domain: ${error}`);
 }
 
 bulkDomainUpdate().catch(err => err);
